7bd64cf31df84be84e2bf2b7c627ed7798c53563
Brian <brianshan@gmail.com>
2026-06-03 12:44:45 +0100

    Update

--- lib/logflare/sources/source/bigquery/pipeline.ex
+++ lib/logflare/sources/source/bigquery/pipeline.ex
@@ -202,8 +202,8 @@ defmodule Logflare.Sources.Source.BigQuery.Pipeline do
       source = Sources.Cache.get_by_id(context.source_id)
 
       # Fetch full LogEvents from ETS — events are still present (marked :ingested)
-      # log_events = fetch_events_from_messages(messages)
-      log_events = []
+      log_events = fetch_events_from_messages(messages)
+      #log_events = []
 
       if source && source.bq_storage_write_api do
         batch_attrs = compute_batch_attrs(log_events, :bq_storage_write)
@@ -311,8 +311,7 @@ defmodule Logflare.Sources.Source.BigQuery.Pipeline do
     :telemetry.span(
       [:logflare, :ingest, :pipeline, :stream_batch],
       %{source_token: source_token},
-      # fn -> execute_bigquery_stream_batch(context, log_events) end
-      fn -> Process.sleep(1000)
+       fn -> execute_bigquery_stream_batch(context, log_events)
        {log_events, %{}}
     end
     )
